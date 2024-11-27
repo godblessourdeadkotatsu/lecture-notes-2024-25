7ce69d1932e4560f0d97252737945fec861ed235
kotatsu <lorenzo.sala969@edu.unito.it>
2024-11-27 16:06:22 +0100

    AAAAAAAAAA

--- SM/Homework 7/regeneration.c
+++ SM/Homework 7/regeneration.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define LAMBDA_ARRIVAL 0.001 // Example lambda value for arrivals
-#define LAMBDA_SHORT 0.005   // Example lambda value for short station
-#define LAMBDA_LONG 0.008    // Example lambda value for long station
-#define MAX_EVENTS 100000
+#define N 10 //max number of machines
+#define HETA_ARRIVAL 3000 // Example lambda value for arrivals
+#define HETA_SHORT 40   // Example lambda value for short station
+#define HETA_LONG 960    // Example lambda value for long station
+#define MAX_EVENTS 1000
+#define BETA 0.2
 
 /*
 Now we prepare all we need to simulate these events.
@@ -22,15 +24,19 @@ typedef struct Event {
 } Event;
 
 int verbose = 0; // the cute logs are off by default.
+int n=N;
 double cycle_waiting_time_short_station = 0;
 double cycle_waiting_time_long_station = 0;
 double total_waiting_time_short = 0;
 double total_waiting_time_long = 0;
 int cycle_count = 0;
+int L_event_count = 0;
+double avg_waiting_time_long = 0;
 
-float lambda_arrival = LAMBDA_ARRIVAL;
-float lambda_short = LAMBDA_SHORT;
-float lambda_long = LAMBDA_LONG;
+float heta_arrival = HETA_ARRIVAL;
+float heta_short = HETA_SHORT;
+float heta_long = HETA_LONG;
+double beta = BETA;
 
 /*
 Now we create a pointer to the even list (that will be an array of the type
@@ -96,12 +102,16 @@ int main(int argc, char *argv[]) {
   // Calculate the time taken and print it
   double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
 
+  // Now calculate our point estimator
+  avg_waiting_time_long=total_waiting_time_long/L_event_count;
+
   printf("\n_______________________________________\n");
-  printf("Simulation complete. ");
+  printf("Simulation complete! ");
   printf("Execution time: %f seconds\n", time_taken);
   printf("Total waiting time in short station: %f\n", total_waiting_time_short);
   printf("Total waiting time in long station: %f\n", total_waiting_time_long);
   printf("Total cycles: %d.\n", cycle_count);
+  printf("Average waiting time for the long station: %f",avg_waiting_time_long);
 
   // Cleanup allocated memory
   cleanup_event_list();
@@ -109,8 +119,8 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-float exponential_random(float lambda) {
-  return lambda * -logf((float)rand() / RAND_MAX);
+float exponential_random(float heta) {
+  return -heta*logf(1- (float)rand() / RAND_MAX);
 }
 
 void add_event(Event new_event) {
@@ -150,6 +160,11 @@ void process_event(Event current_event, int pos) {
   // start by doing the task of verifying whether the event is a regeneration
   // point if it is, then collect the statistics.
   if (RegPoint(current_event)) {
+    /* we accumulate the number of L events in a global accumulator. 
+    Of course, this means that each cycle j will have \nu_j=1 event count, since we always regenerate at the first L event found.
+    This will be used to compute the point estimate of the waiting time.
+    */
+    L_event_count++;
     CollectRegStatistics(&cycle_waiting_time_short_station,
                          &cycle_waiting_time_long_station,
                          &total_waiting_time_short, &total_waiting_time_long,
@@ -171,14 +186,17 @@ void process_event(Event current_event, int pos) {
           : 0;
   if (current_event.type == 'A') {
     // now we create and insert the next arrival
+    if (n>0) {
     Event next_arrival;
-    next_arrival.timestamp =
-        current_event.timestamp + exponential_random(lambda_arrival);
     next_arrival.type = 'A';
+    next_arrival.timestamp =
+        current_event.timestamp + exponential_random(heta_arrival);
     add_event(next_arrival);
+    n--;
     verbose ? printf("Added the next arrival event %c, at time %f\n",
                      next_arrival.type, next_arrival.timestamp)
             : 0;
+            }
 
     // we must check the case in which the queue is empty and in which it isn't.
     // remember, we start from the "pos" position an empty queue means that
@@ -198,14 +216,14 @@ void process_event(Event current_event, int pos) {
     if (last_departure_timestamp == -1.0) {
       current_departure.timestamp =
           current_event.timestamp +
-          exponential_random(lambda_short); // empty queue
+          exponential_random(heta_short); // empty queue
       verbose ? printf("Wow, no one in queue! ") : 0;
     } else {
       current_departure.timestamp =
           last_departure_timestamp +
-          exponential_random(lambda_short); // someone in the queue
+          exponential_random(heta_short); // someone in the queue
     }
-    // insert the fucking event
+    // insert the event
     add_event(current_departure);
     verbose ? printf("Added the departure event %c (corresponding to our "
                      "current event %d) at time %f\n",
@@ -222,7 +240,7 @@ void process_event(Event current_event, int pos) {
     // first, we decide whether this event is going to the long repair station
     // or nah
     float coin_flip = (rand() / (float)RAND_MAX);
-    if (coin_flip <= 0.2) {
+    if (coin_flip <= beta) {
       verbose ? printf("Bad luck! you get a long repair! ") : 0;
       // go to the long station
       Event current_departure_long;
@@ -231,13 +249,13 @@ void process_event(Event current_event, int pos) {
       for (size_t i = pos; i < event_count; i++) {
         if (event_list[i].type == 'L') {
           last_departure_timestamp = event_list[i].timestamp;
-        }
+        } 
       }
       // we got our sentinel
       if (last_departure_timestamp == -1.0) {
         current_departure_long.timestamp =
             current_event.timestamp +
-            exponential_random(lambda_long); // empty queue
+            exponential_random(heta_long); // empty queue
         verbose
             ? printf(
                   "But at least there was no one in queue for the long station")
@@ -245,7 +263,7 @@ void process_event(Event current_event, int pos) {
       } else {
         current_departure_long.timestamp =
             last_departure_timestamp +
-            exponential_random(lambda_long); // someone in the queue
+            exponential_random(heta_long); // someone in the queue
       }
       // insert the event
       add_event(current_departure_long);
@@ -261,14 +279,18 @@ void process_event(Event current_event, int pos) {
       */
       cycle_waiting_time_long_station +=
           current_departure_long.timestamp - current_event.timestamp;
+    } else {
+      n++; //the machine goes back to the source
     }
   } else if (current_event.type == 'L') {
+    n++; //the machine goes back to the source
     verbose ? printf("Found a L event. Nothing to do, move on!\n") : 0;
     // here we only need to collect statistics
   }
   verbose
-      ? printf("Done processing event %c, number %d in the list, at time %f.\n",
-               current_event.type, pos, current_event.timestamp)
+      ? printf("Done processing event %c, number %d in the list, at time %f. In the source there are %d machines left.\n",
+               current_event.type, pos, current_event.timestamp,
+               n)
       : 0;
   verbose ? printf("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n") : 0;
 }
@@ -284,7 +306,7 @@ void cleanup_event_list() {
 int RegPoint(Event current_event) {
   // Regeneration point occurs at a S or L event
   // Returns 1 (true) for regeneration point, 0 (false) otherwise
-  return (current_event.type == 'S' || current_event.type == 'L');
+  return (current_event.type == 'L');
 }
 
 void CollectRegStatistics(double *cycle_waiting_time_short_station,
@@ -306,4 +328,9 @@ void ResetMeasures(double *cycle_waiting_time_long_station,
       0.0; // Reset the cycle-specific waiting time
   *cycle_waiting_time_long_station =
       0.0; // Reset the cycle-specific waiting time
+}
+
+double ComputeConfidenceIntervals(double avg_waiting_time_long) {
+  double s2_A = 1/(cycle_count-1);
+  return 1.96;
 }
